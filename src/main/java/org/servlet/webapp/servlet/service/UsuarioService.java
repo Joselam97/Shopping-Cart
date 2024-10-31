d944fd2335679befab7df5c519ab0260765dd03c
Jose Alvarez <jlam97@hotmail.es>
2024-10-31 12:50:18 -0600

    Implementacion de Metodos a la clase 'UsuarioServiceImpl' y su interface 'UsuarioService'

--- src/main/java/org/servlet/webapp/servlet/service/UsuarioService.java
+++ src/main/java/org/servlet/webapp/servlet/service/UsuarioService.java
@@ -2,8 +2,17 @@ package org.servlet.webapp.servlet.service;
 
 import org.servlet.webapp.servlet.models.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioService {
     Optional<Usuario> login(String username, String password);
+
+    List<Usuario> listar();
+
+    Optional<Usuario> porId(Long id);
+
+    void guardar(Usuario usuario);
+
+    void eliminar(Long id);
 }
